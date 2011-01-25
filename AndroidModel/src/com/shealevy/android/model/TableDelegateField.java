ab1e9822700956fe2a9d4a25d7011ab8e5a960fa
Shea Levy <shea@shealevy.com>
2011-01-25 16:06:33 -0500

    Got TableDelegateField defaultValue() when int spec to pass, which caused GettingUnsetFields feature to pass again

--- AndroidModel/src/com/shealevy/android/model/TableDelegateField.java
+++ AndroidModel/src/com/shealevy/android/model/TableDelegateField.java
@@ -1,11 +1,12 @@
 package com.shealevy.android.model;
 
 public class TableDelegateField<T, U> {
+	private U defaultValue;
 	protected TableDelegateField(String value, U defaultValue) {
-		
+		this.defaultValue = defaultValue;
 	}
 	
 	public U defaultValue() {
-		return (U) "Test";
+		return defaultValue;
 	}
 }
