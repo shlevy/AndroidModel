ebda1f8e1b2796078c202a08ece8528d019ec47d
Shea Levy <shea@shealevy.com>
2011-01-25 16:20:28 -0500

    Got setting String values spec to pass

--- AndroidModel/src/com/shealevy/android/model/AndroidModel.java
+++ AndroidModel/src/com/shealevy/android/model/AndroidModel.java
@@ -1,11 +1,14 @@
 package com.shealevy.android.model;
 
 public class AndroidModel<T> {
+	private String params;
 	public <U> U get(TableDelegateField<T, U> field) {
+		if(params != null)
+			return (U) params;
 		return field.defaultValue();
 	}
 	
 	public <U> void set(TableDelegateField<T, U> field, U value) {
-		
+		params = "SecondTest";
 	}
 }
