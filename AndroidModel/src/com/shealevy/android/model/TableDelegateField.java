2234229e0f975417aadf1a8dd07ce923352c7594
Shea Levy <shea@shealevy.com>
2011-01-25 15:50:41 -0500

    Created TableDelegateField(String value, U defaultValue) constructor

--- AndroidModel/src/com/shealevy/android/model/TableDelegateField.java
+++ AndroidModel/src/com/shealevy/android/model/TableDelegateField.java
@@ -1,6 +1,10 @@
 package com.shealevy.android.model;
 
 public class TableDelegateField<T, U> {
+	protected TableDelegateField(String value, U defaultValue) {
+		
+	}
+	
 	public U defaultValue() {
 		return null;
 	}
