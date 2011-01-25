5bc2ac1b270c5926b7588948f01776b232a71333
Shea Levy <shea@shealevy.com>
2011-01-24 23:36:27 -0500

    Set up AndroidMocks, added spec for uninitialized byte

--- AndroidModelTestAppTest/src/com/shealevy/android/model/test/app/test/spec/AndroidModelTest.java
+++ AndroidModelTestAppTest/src/com/shealevy/android/model/test/app/test/spec/AndroidModelTest.java
@@ -1,5 +1,9 @@
 package com.shealevy.android.model.test.app.test.spec;
 
+import com.shealevy.android.model.AndroidModel;
+import com.shealevy.android.model.TableDelegateField;
+import com.shealevy.android.model.test.app.test.TestTableDelegate;
+
 import android.test.AndroidTestCase;
 
 public class AndroidModelTest extends AndroidTestCase {
@@ -16,4 +20,20 @@ public class AndroidModelTest extends AndroidTestCase {
 		super.tearDown();
 	}
 
+	// Describe get(TableDelegateField<T> field)
+	public void testItReturns0WhenAByteIsRequested() {
+		ByteField b = new ByteField();
+		AndroidModel<TestTableDelegate> model = new AndroidModel<TestTableDelegate>();
+		assertEquals((byte) 0, model.get(b));
+	}
+	
+	private class ByteField implements TableDelegateField<TestTableDelegate> {
+		public String getValue() {
+			return "byte";
+		}
+		
+		public Class<?> getType() {
+			return byte.class;
+		}
+	}
 }
