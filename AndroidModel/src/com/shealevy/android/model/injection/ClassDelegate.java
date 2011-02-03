02510e20d64888a3915823a491182d6930562092
Shea Levy <shea@shealevy.com>
2011-02-03 15:11:04 -0500

    Set up ClassDelegate with default delegate methods and newInstance() method

--- AndroidModel/src/com/shealevy/android/model/injection/ClassDelegate.java
+++ AndroidModel/src/com/shealevy/android/model/injection/ClassDelegate.java
@@ -1,9 +1,21 @@
 package com.shealevy.android.model.injection;
 
 public class ClassDelegate<T> {
+	private Class<T> delegate;
 	
 	public ClassDelegate(Class<T> delegate) {
-		
+		setDelegate(delegate);
 	}
 
+	public Class<T> getDelegate() {
+		return delegate;
+	}
+	
+	public void setDelegate(Class<T> delegate) {
+		this.delegate = delegate;
+	}
+	
+	public T newInstance() throws IllegalAccessException, InstantiationException {
+		return getDelegate().newInstance();
+	}
 }
