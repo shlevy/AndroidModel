7280c787fcd3c84c10dd4a3a0f204317daf07e9d
Shea Levy <shea@shealevy.com>
2011-02-03 19:21:57 -0500

    Got ConstructorSetsTheTableDelegateToTheProvidedTableDelegateWhenAHashMapClassIsProvidedAsAClassDelegate spec to pass, which got SettingTheTableDelegateAtConstructionAndProvidingAHashMapSubClassClassDelegate scenario to pass

--- AndroidModel/src/com/shealevy/android/model/AndroidModel.java
+++ AndroidModel/src/com/shealevy/android/model/AndroidModel.java
@@ -44,6 +44,7 @@ public class AndroidModel<T extends TableDelegate> {
 
 	public AndroidModel(T tableDelegate,
 			ClassDelegate<? extends HashMap> hashMapClass) {
+		setTableDelegate(tableDelegate);
 		constructParams(hashMapClass);
 	}
 
